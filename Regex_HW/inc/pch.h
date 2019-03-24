4bb044c8c15075b53e2fb5c7460e28daad495eae
Ben Hoffman <benjamin.hoffman.dev@gmail.com>
2019-03-24 12:06:05 -0400

    inital project creation with CMake

---
+++ Regex_HW/inc/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/**
+* @brief    Precompiled header for this program with common
+*           libraries/defines that are used
+*
+* @author   Ben Hoffman ( bah8892@rit.edu )
+*/
+
+#include <iostream>
+#include <regex>
---
+++ Regex_HW/src/Main.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+/**
+* @brief    Entry point of the program
+*/
+int main( int argc, char* argv [] )
+{
+    std::cout << "Hello regex!" << std::endl;
+
+    return 0;
+}
